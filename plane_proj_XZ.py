88e51029fa40039c6fe3b7da6aa4d2da557ba19a
AndreaCaprotti <andrea.caprotti@univie.ac.at>
2025-09-08 16:17:12 +0200

    Proper comments included

--- plane_proj_XZ.py
+++ plane_proj_XZ.py
@@ -47,38 +47,47 @@ goldenratio=1.618
 imagewidth = 7 # 
 # -
 
-import opt_variance_coef as ocf
-import states_functions as sf
-import povm_functions as pf
+# homebrewed modules for the actual variance optimisation and handling of different functions
+import opt_variance_coef as ocf  # variance optimisation related functions
+import states_functions as sf    # state and observables related functions
+import povm_functions as pf      # POVM generation and handling related functions
 
 # +
-single_povm = pf.pauli_povm_single('X','Z')
-density = 30
-N_max = 2
+# global variables
+single_povm = pf.pauli_povm_single('X','Z') # single qubit plane POVM
 
+density = 30  # number of different projectors considered
+
+N_min = 1     # minimal dimension considered
+N_max = 2     # maximal dimension considered
+
+# saving directory (created if non-existing)
 directory= 'plane_proj_XZ'
 os.system(f'mkdir {directory}')
 # -
 
+# # Cycle over different dimensions
+
 for N in range(1,N_max+1):
     filename = directory+f'_{N}' # just to be clear, file name is the same as directory
+    # tensor POVM definition
     povm = pf.tensor_same(single_povm, N)
-
     [pcm,bm] = pf.povm_coef_matrix(povm)
     can_em = sc.linalg.pinv(pcm) # returns the Moore-Penrose pseudo-inverse (canonical inverse)
 
-    var_vec = []
-    # canonical coefficients - (this is actually the same for observables, 
-    single_obs = sf.qubit(0,0)
+    # canonical inversion
+    single_obs = sf.qubit(0,0)          # we've seen that it's the same for all projectors of the same size (for the plane case at least)
     obs = pf.tensor_same(single_obs,N)
-    cc = can_em.T@(sf.flatten_in_basis(obs,bm))
-
+    cc = can_em.T@(sf.flatten_in_basis(obs,bm)) # canonical coefficients
     [var_can, rho_can] = ocf.fix_coef_var_optimisation(pcm, bm, cc) # canonical maximal variance
     np.save(f'{directory}/{filename}_can',var_can)
 
+    # optimal upper bound on variance
+    var_vec = []
     for j in range(density):
         theta = j*np.pi/(2*density) # angle runs between 0 and \pi
         phi = 0                     # we consider projectors on the prime meridian
+        
         singleobs = sf.qubit(theta, phi)
         obs = pf.tensor_same(singleobs, N) # N-tensor of same observable
         # optimal variance
